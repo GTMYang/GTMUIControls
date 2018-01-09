44bece9fa31e1799d4c037ca7a9f5edec031750b
GTMYang <289135816@qq.com>
2018-01-09 13:23:40 +0800

    Initial Commit

---
+++ GTMUIControlsTests/GTMUIControlsTests.swift
@@ -0,0 +1,36 @@
+//
+//  GTMUIControlsTests.swift
+//  GTMUIControlsTests
+//
+//  Created by 骆扬 on 2018/1/9.
+//  Copyright © 2018年 LY. All rights reserved.
+//
+
+import XCTest
+@testable import GTMUIControls
+
+class GTMUIControlsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
